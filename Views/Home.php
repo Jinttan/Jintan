dd9ce1185dd8c604e0ef82e3dde6c7c2311a6616
Jinttan <eduardovcpz@hotmail.es>
2016-09-14 12:06:39 -0600

    carpetas

---
+++ Views/Home.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Eduardo
+ * Date: 13/9/2016
+ * Time: 7:36 PM
+ */
+$name = 'Eduardo Venegas ';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HOLA !</title>
+</head>
+<body>
+Hello <?php $name; ?> welcome to Jintan :3
+</body>
+</html>
